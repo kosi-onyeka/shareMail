c68aafdf85ef06bb9c10242eb6eaac061b566c61
Kosi Onyeka <kosi@Kosis-MacBook-Pro.local>
2024-03-30 14:01:59 -0400

    first commit

---
+++ EmailService.py
@@ -0,0 +1,40 @@
+import imaplib
+import email
+import html2text
+import os
+
+class EmailService:
+    def __init__(self):
+        self.emailAddress = "kosionyeka@yahoo.com"
+        self.__connectToEmail()
+
+    
+    def __connectToEmail(self): 
+        emailPassword = os.environ['YAHOO_PASSWORD']
+        yahoo_imap_server = "imap.mail.yahoo.com"
+        yahoo_imap_port = 993
+        try:
+            self.mailServer = imaplib.IMAP4_SSL(yahoo_imap_server, yahoo_imap_port,)
+        except imaplib.IMAP4.error as e:
+            print(f"Failed to connect to the server: {e}")
+            exit()
+        self.mailServer.login(self.emailAddress, emailPassword)
+        self.mailServer.select('INBOX')
+        
+
+    def getHuluLogin(self):
+        status, messages = self.mailServer.search(None, "FROM", "Hulu")
+        if status == "OK":
+            # Get the latest message number
+            latest_email_id = messages[0].split()[-1]
+
+            # Fetch and print details of the most recent email
+            status, msg_data = self.mailServer.fetch(latest_email_id, "(RFC822)")
+            for response in msg_data:
+                if type(response) is tuple:
+                    myMsg = email.message_from_bytes(response[1])
+                    body = myMsg.get_payload(decode=True).decode("utf-8")
+                    plainTextContent = html2text.html2text(body)  
+                    plainTextContent = plainTextContent.split('.')
+                    self.verification_code = plainTextContent[0]
+
---
+++ twilioApp.py
@@ -0,0 +1,39 @@
+from flask import Flask, request
+from twilio.rest import Client
+from EmailService import EmailService
+import os
+
+app = Flask(__name__)
+account_sid = os.environ['ACCOUNT_SID']
+auth_token = os.environ['AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+@app.route('/testAPI', methods=['GET'])
+def testAPI():
+    mailService = EmailService()
+    mailService.getHuluLogin()
+    response = mailService.verification_code
+    return(response)
+
+@app.route('/sms', methods=['GET'])
+def sms():
+    number = request.form['From']
+    message_body = request.form['Body']
+    
+    if message_body.lower() == "hulu".lower():
+        mailService = EmailService()
+        mailService.getHuluLogin()
+        response = mailService.verification_code
+    else:
+        response = 'Wrong request. Reach out to Kosi'
+
+    message = client.messages.create(
+        body = response,
+        from_='+18556191591',
+        to= number
+    )
+    return str(response)
+
+
+if __name__ == '__main__':
+    app.run(port=5002, debug=True)
